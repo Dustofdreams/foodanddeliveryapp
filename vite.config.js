23cf1564e5f5d49884735481799f67a70bc0fc9e
DustOfDreams <52716391+Dustofdreams@users.noreply.github.com>
2023-03-28 12:57:01 +0300

    logo path changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/foodanddeliveryapp/',
-  plugins: [react()],
+  plugins: [react({
+    template: {
+      transformAssetUrls: {
+          includeAbsolute: false
+      },
+}})],
 })
